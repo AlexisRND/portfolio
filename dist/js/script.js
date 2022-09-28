22455d7865584cf42fdfb1ad004986d0cf46978e
Alexandr <bormotov1994@bk.ru>
2022-09-28 19:15:35 +0300

    count js

--- dist/js/script.js
+++ dist/js/script.js
@@ -10,3 +10,11 @@ closeElem.addEventListener('click', () => {
     menu.classList.remove('active')
 });
 
+const counters = document.querySelectorAll('.ghraph__item-digit'),
+    lines = document.querySelectorAll('.ghraph__item-line span');
+
+counters.forEach( (item, i) => {
+    lines[i].style.width = item.innerHTML;
+})
+
+
