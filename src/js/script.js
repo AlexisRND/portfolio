24d18a59a975d27759ee5587e727c8b1a631adf6
Alexandr <bormotov1994@bk.ru>
2022-10-09 03:21:53 +0300

    media first 3 section

--- src/js/script.js
+++ src/js/script.js
@@ -18,3 +18,68 @@ counters.forEach( (item, i) => {
 })
 
 
+$(document).ready(function(){
+    $('.contacts__form').validate({
+        rules: {
+            name: {
+                required: true,
+                minlength: 2
+              },
+            email: {
+                required: true,
+                email: true
+            },
+            checkbox: {
+                required: true
+            }
+        },
+        messages: {
+            name: {
+                required:  "Пожалуйста, введите своё имя",
+                minlength: jQuery.validator.format("Введите минимум {0} символа!")
+              },
+            email: {
+              required: "Пожалуйста, введите свою почту",
+              email: "Неправильно введен адрес почты"
+            },
+            checkbox: {
+                required: "Пожалуйста, ознакомьтесь с политикой в отношении обработки персональных данных"
+            }
+          }
+    });
+
+
+    $('form').submit(function(e) {
+        e.preventDefault();
+
+        if (!$(this).valid())
+        return;
+        
+        $.ajax({
+            type: "POST",
+            url: "mailer/smart.php",
+            data: $(this).serialize()
+        }).done(function() {
+            $(this).find("input").val("");
+            $('.overlay, .modal').fadeIn('slow');
+            $('.modal__close').on('click', function() {
+                $('.overlay, modal').fadeOut('slow')
+            });
+
+            $('form').trigger('reset');
+        })
+        
+        return false;
+    });
+    
+
+    //modal
+
+    $('.modal__close').on('click', function() {
+        $('.overlay, modal').fadeOut('slow')
+    })
+
+
+
+
+});
